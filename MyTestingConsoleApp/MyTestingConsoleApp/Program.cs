195640710224ac776775c32b09036b38a054f4d9
juan salazaer <sjuan458@gmail.com>
2019-01-14 22:56:14 -0500

    some delegate tries

--- MyTestingConsoleApp/MyTestingConsoleApp/Program.cs
+++ MyTestingConsoleApp/MyTestingConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+
+delegate void myDelegate();
 
 namespace MyTestingConsoleApp
 {
@@ -6,7 +9,18 @@ namespace MyTestingConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEnumerable<int> result = GetAllNumbersLessThanFive(
+                new[] { 2, 7, 3, 9, 5, 7, 1, 8 });
+            foreach (int n in result)
+                Console.WriteLine(n);
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> GetAllNumbersLessThanFive(IEnumerable<int> numbers)
+        {
+            foreach (int number in numbers)
+                if (number < 5)
+                    yield return number;
         }
     }
 }
