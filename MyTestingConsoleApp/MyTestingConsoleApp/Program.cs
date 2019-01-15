51661e4326bbdcf1c67aaff8ae17ade79c3ca240
juan salazaer <sjuan458@gmail.com>
2019-01-14 21:23:06 -0500

    Initial Commit

---
+++ MyTestingConsoleApp/MyTestingConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTestingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
