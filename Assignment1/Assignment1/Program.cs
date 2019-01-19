06e5d378b41834ad098bced234e47ee7086d0a18
Juan Salazar <sjuan458@gmail.com>
2019-01-18 21:25:43 -0500

    Added assignment 1 folder(solution)

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
